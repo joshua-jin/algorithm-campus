10af41e78cec94d4e2eff2a692b3acad2501b385
lumingna <lumingna@baidu.com>
2016-02-02 00:51:21 +0800

    solve remove linked list element problem

---
+++ lulu/remove-linked-list-elements.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    # @param head, a ListNode
+    # @param val, an integer
+    # @return a ListNode
+
+    # Non-recursive way
+    def removeElements(self, head, val):
+        # Write your code here
+        i = head
+        while i is not None:
+            if i.val != val:
+                head = i
+                break
+            i = i.next
+        if i is None:
+            return None
+        former = head
+        i = head.next
+        while i is not None:
+            if i.val == val:
+                former.next = i.next
+            else:
+                former = i
+            i = i.next
+        return head
+
+    # Recursive way
+    def removeElements(self, head, val):
+        if head is None:
+            return None
+        if head.val == val:
+            return self.removeElements(head.next, val)
+        i = self.removeElements(head.next, val)
+        head.next = i
+        return head
