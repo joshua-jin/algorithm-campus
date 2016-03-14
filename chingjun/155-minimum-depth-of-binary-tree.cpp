8ab21890a1e74d2f30eac62634cff08b773804c2
Lau Ching Jun <lauchingjun@hotmail.com>
2016-03-14 13:53:17 +0800

    Create 155-minimum-depth-of-binary-tree.cpp

---
+++ chingjun/155-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    int minDepth(TreeNode *root) {
+        if (root == NULL) return 0;
+        if (root->left == NULL) return minDepth(root->right) + 1;
+        else if (root->right == NULL) return minDepth(root->left) + 1;
+        else return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
